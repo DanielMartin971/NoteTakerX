c18ac54bf517dab7fd89d7cefd143ada7655adb4
Daniel <danielmartin971337@gmail.com>
2022-11-30 00:31:40 -0800

    added in express and the npm install. made a routes folder with index blank at the moment. it can port to localhost 3000

--- Develop/server.js
+++ Develop/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const fs      = require('fs');
 const path    = require('path');
 
-const app = express();
-const PORT = process.env.port || 3001;
+const app  = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use
+app.use(express.static(__dirname));
+
+require('./routes/index');
 
 app.listen(PORT, function(){
     console.log('App listening on PORT: ' + PORT);
