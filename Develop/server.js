eeec0d2fdd7d1a010ea71f554ed6e807e7c7cb30
Daniel <danielmartin971337@gmail.com>
2022-11-30 17:26:57 -0800

    added a function for creating and deleting a note. There's a bug though that it creates a object inside a object. but it deletes and adds a new note regardless. got the thing to function after too much testing.

--- Develop/server.js
+++ Develop/server.js
@@ -27,6 +27,49 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 1)
+            );
+
+            break;
+        }
+    }
+}
+
+function createNote(body, notesArray) {
+    const newNote = body;
+
+    body.id = notesArray[0];
+    notesArray.push(newNote);
+
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 1)
+    );
+
+    return newNote;
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, notes);
+    console.log('This note was deleted: ' + req.params.id);
+    res.json(true);
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = createNote(req.body, notes);
+    console.log('This note was added: ' + req.params.title);
+    res.json(newNote);
+});
+
 app.listen(PORT, () => {
     console.log('App listening on PORT: ' + PORT);
 });
